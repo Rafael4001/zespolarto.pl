8614a55d3ff670188bf9921367fecbef28f821c4
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2020-04-19 14:52:29 +0200

    reservation crud

--- backend/src/reservations/reservation.model.ts
+++ backend/src/reservations/reservation.model.ts
@@ -4,9 +4,13 @@ import { IsString } from "class-validator";
 export const ReservationSchema = new mongoose.Schema({
   title: {
     type: String
+  },
+  city: {
+    type: String
   }
 });
 
 export class Reservation {
   @IsString() readonly title: string;
+  @IsString() readonly city: string;
 }
