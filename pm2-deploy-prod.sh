70ea72b75412362857f40df2ec8966af61ef735b
Sebastian Cytawa <sebastian.cytawa@edulab.io>
2020-04-19 13:57:38 +0200

    nginx config

--- pm2-deploy-prod.sh
+++ pm2-deploy-prod.sh
@@ -3,7 +3,7 @@ cp my-app.conf /etc/nginx/conf.d
 sudo nginx -t
 sudo service nginx restart
 git pull origin master
-pm2 remove 1
-pm2 remove 2
+pm2 delete "be"
+pm2 delete "fe"
 pm2 start npm --name "be" -- run start
 pm2 start npm --name "fe" -- run start
