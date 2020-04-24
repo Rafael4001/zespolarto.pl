e0a1ba660adf77cb6537903c30872e358dfcd8cf
Rafał Ciesielczuk <rafal.ciesielczuk@gmail.com>
2020-04-24 13:53:29 +0200

    step

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 //server
-// export  const API_URL = "http://zespolarto.pl/api/reservations";
+export  const API_URL = "http://zespolarto.pl/api/reservations";
 
 // localnie z uzyciem servera
-export const API_URL="https://cors-anywhere.herokuapp.com/http://zespolarto.pl/api/reservations";
+// export const API_URL="https://cors-anywhere.herokuapp.com/http://zespolarto.pl/api/reservations";
 
 // localna baza danych
 export const API_URL_LOCAL="http://localhost:5000/api/reservations";
