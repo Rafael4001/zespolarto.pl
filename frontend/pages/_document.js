21f5fa334c6bbee3a3756355ebcbaa27777afa0d
rafal.ciesielczuk <rafal.ciesielczuk@gmail.com>
2021-08-04 15:28:55 +0200

    Styles queue

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -8,10 +8,42 @@ import Document,
 } from 'next/document'
 import theme from '../config/theme';
 import {FAVICON} from "../src/constants";
-import { ServerStyleSheets } from '@material-ui/core/styles';
+// import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets } from "@material-ui/styles";
 
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const styledComponentsSheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props =>
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            )
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <Fragment>
+            {initialProps.styles}
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
+          </Fragment>
+        )
+      };
+    } finally {
+      styledComponentsSheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html lang="pl">
@@ -39,50 +71,50 @@ class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: App => props => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      <Fragment key="styles">
-        {initialProps.styles}
-        {/*{sheets.getStyleElement()}*/}
-      </Fragment>,
-    ],
-  };
-};
+// MyDocument.getInitialProps = async (ctx) => {
+//   // Resolution order
+//   //
+//   // On the server:
+//   // 1. app.getInitialProps
+//   // 2. page.getInitialProps
+//   // 3. document.getInitialProps
+//   // 4. app.render
+//   // 5. page.render
+//   // 6. document.render
+//   //
+//   // On the server with error:
+//   // 1. document.getInitialProps
+//   // 2. app.render
+//   // 3. page.render
+//   // 4. document.render
+//   //
+//   // On the client
+//   // 1. app.getInitialProps
+//   // 2. page.getInitialProps
+//   // 3. app.render
+//   // 4. page.render
+//
+//   // Render app and page and get the context of the page with collected side effects.
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
+//
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: App => props => sheets.collect(<App {...props} />),
+//     });
+//
+//   const initialProps = await Document.getInitialProps(ctx);
+//
+//   return {
+//     ...initialProps,
+//     // Styles fragment is rendered after the app and page rendering finish.
+//     styles: [
+//       <Fragment key="styles">
+//         {initialProps.styles}
+//         {/*{sheets.getStyleElement()}*/}
+//       </Fragment>,
+//     ],
+//   };
+// };
 
 export default MyDocument;
